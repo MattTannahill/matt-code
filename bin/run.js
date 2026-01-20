d093eb50356fa74932dec1cb32a4c6f6bbff9c1f
Matt Tannahill <MattTannahill@users.noreply.github.com>
2026-01-20 13:57:20 -0500

    Make UI the default command

--- bin/run.js
+++ bin/run.js
@@ -3,7 +3,8 @@
 import {execute} from '@oclif/core'
 
 // Avoid top-level await which can trigger warnings in some environments
-void execute({dir: import.meta.url}).catch((err) => {
+const args = process.argv.slice(2).length === 0 ? ['ui'] : process.argv.slice(2);
+void execute({dir: import.meta.url, args}).catch((err) => {
   // eslint-disable-next-line no-console
   console.error(err);
   process.exitCode = 1;
